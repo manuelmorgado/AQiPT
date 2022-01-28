c0baa3ce1513081fa7914b872ea2e61ba29f364c
Manuel Morgado <2m2706@gmail.com>
2022-01-28 12:43:19 +0100

    new directory structure for new release
    Modules-DAQ-control-datalogger

---
+++ modules/daq/AQiPT-daq.py
@@ -0,0 +1,5 @@
+#Atomic Quantum information Processing Tool (AQIPT) - DAQ module
+
+# Author: Manuel Morgado. Universite de Strasbourg. Laboratory of Exotic Quantum Matter - CESQ
+# Created: 2021-04-08
+# Last update: 2021-04-15
