66d568d16c78650a1b18972b7666e723fc60af3a
Manuel Morgado <2m2706@gmail.com>
2022-02-07 16:39:11 +0100

    updating module

---
+++ modules/interface/AQiPTgui.py
@@ -0,0 +1,6 @@
+#Atomic Quantum information Processing Tool (AQIPT) - Directory module
+
+# Author(s): Manuel Morgado. Universite de Strasbourg. Laboratory of Exotic Quantum Matter - CESQ
+# Contributor(s): 
+# Created: 2021-04-08
+# Last update: 2022-02-07
