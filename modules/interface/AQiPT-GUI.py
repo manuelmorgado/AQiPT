667964aa4e29f7c562b0a1a29a4d9ee0f26bd7a0
Manuel Morgado <2m2706@gmail.com>
2022-02-07 16:39:41 +0100

    deleting old version

--- modules/interface/AQiPT-GUI.py
+++
@@ -1,5 +0,0 @@
-#Atomic Quantum information Processing Tool (AQIPT) - GUI module
-
-# Author: Manuel Morgado. Universite de Strasbourg. Laboratory of Exotic Quantum Matter - CESQ
-# Created: 2021-04-08
-# Last update: 2021-04-15
